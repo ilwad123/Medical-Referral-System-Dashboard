d415946e026cb0c392e428491fb31e141f87a9b7
ilwad123 <ilwadabdi234@gmail.com>
2024-02-29 21:56:30 +0000

    search,reffered filter and colour done

--- app.py
+++ app.py
@@ -36,15 +36,34 @@ def viewpatient():
             datarows.append(data)
     if request.method == 'POST':
             datarows = [row for row in datarows if referral_filter == 'All' or referral_filter == row[17]]
-            # datarows = [row for row in datarows if searchQuery ==row[0]]
-            
-
+            # datarows = [row for row in datarows if searchQuery ==row[0]
    
     return render_template('patient.html', datarows=datarows)
 
 
 
-
+@app.route('/search',methods=['GET','POST'])
+def search():
+    searchQuery=request.form.get('searchQuery')
+    datarows = []
+    absolute_path = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(absolute_path, "Feeding Dashboard data.csv"), 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)  # Skip header
+        for line in reader:
+            data = [item.strip() if item.strip() != "" else "None" for item in line]
+            ref = data[17]
+            
+            if ref == "0":
+                data[17] = "Not Referred"
+            else:  
+                data[17] = "Referred"
+            
+            datarows.append(data)
+    if request.method == 'POST':
+            datarows = [row for row in datarows if searchQuery == row[0]]
+   
+    return render_template('patient.html', datarows=datarows)
 
 
 if __name__ == '__main__':
