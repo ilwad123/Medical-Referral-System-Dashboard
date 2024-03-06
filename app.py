7a44ac7012a394325e0a15f65dba8062968a48f1
ilwad123 <ilwadabdi234@gmail.com>
2024-03-06 20:59:26 +0000

    pages added need to stop the overlap scrollbar added

--- app.py
+++ app.py
@@ -36,53 +36,29 @@ def viewpatient():
         next(reader)  # Skip header
         for line in reader:
             data = [item.strip() if item.strip() != "" else "None" for item in line]
-            ref = data[17]
-            encounterid=data[0]
-            
+            ref = data[17]            
             if ref == "0":
                 data[17] = "Not Referred"
             else:  
                 data[17] = "Referred"
             
-            datarows.append(data)
+            datarows.append(data)        
+            
     if request.method == 'POST':
+        if searchQuery:
+            datarows = [row for row in datarows if searchQuery == row[0]]
+        elif referral_filter:
             datarows = [row for row in datarows if referral_filter == 'All' or referral_filter == row[17]]
-            # datarows = [row for row in datarows if searchQuery ==row[0]
-    page = request.args.get(get_page_parameter(), type=int, default=1)
-    per_page = 10  # Adjust per_page value as needed
-    offset = (page - 1) * per_page
-    paginated_data = datarows[offset: offset + per_page]
-    
-    pagination = Pagination(page=page, total=len(datarows), per_page=per_page, css_framework='bootstrap4')
+            
+            
+    per_page=15
+    page = request.args.get('page', 1, type=int)
+    pagination = Pagination(page=page, total=len(datarows),per_page=per_page)
+    first_page=(page - 1) * per_page 
+    last_page=first_page + per_page
+    paginated_data = datarows[first_page:last_page]
 
     return render_template('patient.html', datarows=paginated_data, pagination=pagination)
-
-
-
-@app.route('/search',methods=['GET','POST'])
-def search():
-    searchQuery=request.form.get('searchQuery')
-    datarows = []
-    absolute_path = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(absolute_path, "Feeding Dashboard data.csv"), 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)  # Skip header
-        for line in reader:
-            data = [item.strip() if item.strip() != "" else "None" for item in line]
-            ref = data[17]
-            
-            if ref == "0":
-                data[17] = "Not Referred"
-            else:  
-                data[17] = "Referred"
-            
-            datarows.append(data)
-    if request.method == 'POST':
-            datarows = [row for row in datarows if searchQuery == row[0]]
-   
-    return render_template('patient.html', datarows=datarows)
-
-
 
 
 if __name__ == '__main__':
